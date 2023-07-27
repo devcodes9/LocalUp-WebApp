ae5b237c3bc29a7692019846a8e8097ef5f9e063
Dev Samir Dalia <devdalia9@gmail.com>
2023-07-27 22:38:00 +0530

    initial change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
     process.env.NODE_ENV === "development"
       ? process.env.REACT_APP_BACKEND_URL_DEVELOPMENT
       : process.env.REACT_APP_BACKEND_URL_PRODUCTION;
-
+  console.log(url)
   return (
     <div>
       <Button>LocalUp</Button>
